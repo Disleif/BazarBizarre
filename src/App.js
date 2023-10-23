43d6e40c3bd581134e99b7a9809b9441e2a66293
Disleif <58111527+Disleif@users.noreply.github.com>
2023-10-23 16:56:21 +0200

    switch state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Header from './components/header/header';
 import Jeu from './components/body/jeu';
 import Regles from './components/body/regles';
 
-
 function App() {
-
   const [jeu, setJeu] = useState(true);
   const [regles, setRegles] = useState(false);
-  
+
+  const switchToJeu = () => {
+    setJeu(true);
+    setRegles(false);
+  };
+
+  const switchToRegles = () => {
+    setJeu(false);
+    setRegles(true);
+  };
+
   return (
     <div className="App">
-      <Header jeu={jeu} regles={regles}/>
+      <Header jeu={jeu} regles={regles} switchToJeu={switchToJeu} switchToRegles={switchToRegles} />
 
       {jeu ? <Jeu /> : null}
       {regles ? <Regles /> : null}
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 
 
-const Header = ({jeu, regles}) => {
+const Header = ({jeu, regles, switchToJeu, switchToRegles}) => {
     return (
         <div className="text-white bg-[#20242c] justify-between flex">
-            <div className={`m-4 ml-5 py-3 px-5 bg-gray-900 hover:bg-black ${jeu ? 'bg-green' : ''} rounded-md cursor-pointer`}>
-                Règles
+            <div 
+            className={`m-4 ml-5 py-3 px-5 rounded-md cursor-pointer hover:bg-green-700
+            ${jeu ? 'bg-green-700' : 'bg-gray-900'}`}
+            onClick={switchToJeu}
+            >
+                Jeu
             </div>
 
             <div className="m-4 py-3 font-serif font-bold">
                 Bazar Bizarre - Entraînement
             </div>
 
-            <div className="m-4 mr-5 py-3 px-5 bg-gray-900 hover:bg-black rounded-md cursor-pointer">
-                Bouton
+            <div 
+            className={`m-4 mr-5 py-3 px-5 rounded-md cursor-pointer hover:bg-green-700 
+            ${regles ? 'bg-green-700' : 'bg-gray-900'}`}
+            onClick={switchToRegles}
+            >
+                Règles
             </div>
 
 
